071accdfb47f2390def976c0730836f155b9be74
DM-IN-GG-23 <digitalmain@DM-IN-GG-23.local>
2024-12-24 06:28:09 +0530

    The multiplication method is added

--- HelloWorldProject/src/com/example/helloworld/Main.java
+++ HelloWorldProject/src/com/example/helloworld/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	
 	public static void main(String []args) {
 		System.out.println("Hello Java world");
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
 	}
 
 }
--- HelloWorldProject/src/com/example/helloworld/MultiplicationTable.java
+++ HelloWorldProject/src/com/example/helloworld/MultiplicationTable.java
@@ -1,8 +1,9 @@
 package com.example.helloworld;
 
 public class MultiplicationTable {
-	public static void main(String [] args) {
+	void print() {
 		System.out.printf("The multiplication table of %d is: " , 5).println();
+		
 		for ( int i = 1 ; i <=10; i++) {
 			System.out.printf(" % d x %d = %d",  5, i, 5*i).println();
 		}
