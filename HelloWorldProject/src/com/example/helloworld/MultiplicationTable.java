f3987bcf6db4da72e634e6aa17304482bfc4f1a5
DM-IN-GG-23 <digitalmain@DM-IN-GG-23.local>
2024-12-24 06:15:54 +0530

    Multiplication table is added

---
+++ HelloWorldProject/src/com/example/helloworld/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class MultiplicationTable {
+	public static void main(String [] args) {
+		System.out.printf("The multiplication table of %d is: " , 5).println();
+		for ( int i = 1 ; i <=10; i++) {
+			System.out.printf(" % d x %d = %d",  5, i, 5*i).println();
+		}
+		
+	}
+
+}
